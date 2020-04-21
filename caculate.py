f448d46c57143c88d6a2d8e031190caaf0b24902
St-Ren <b06705009@ntu.edu.tw>
2020-04-21 20:54:51 +0800

    caculate

--- caculate.py
+++ caculate.py
@@ -13,7 +13,11 @@ def toNum(word,ns,hashtag):
 		else:
 			return None
 	if 'const' in word:
-		n = word.split('_')[1]
+		ns = word.split('_')
+		if len(ns) == 3:
+			return float(ns[1]+'.'+ns[2])
+		else:
+			n = ns[1]
 		if n == 'pi':
 			return math.pi
 		return float(n)
@@ -32,7 +36,9 @@ def caculate(op,ns):
 		if num1 == None:
 				return 'number invalid'
 		if ops[i] == 'inverse':
-			hashtag.append(1/num1)
+			if num1 == 0:
+				return 'inverse 0'
+			hashtag.append(1 / num1)
 		elif ops[i] == 'negate':
 			hashtag.append(-num1)
 		elif ops[i] == 'sqrt':
@@ -48,6 +54,12 @@ def caculate(op,ns):
 		elif ops[i] == 'negate_prob':
 			hashtag.append(1 - num1)
 		elif ops[i] == 'factorial':
+			if num1 > 10 ** 4:
+				return 'fac big num {}'.format(num1)
+			if num1 < 0:
+				return 'neg factorial'
+			if num1 != int(num1):
+				return 'float factorial'
 			hashtag.append(math.factorial(num1))
 		elif ops[i] == 'floor':
 			hashtag.append(math.floor(num1))
@@ -61,13 +73,20 @@ def caculate(op,ns):
 			hashtag.append(math.pow(num1,1/3))
 		elif ops[i] == 'volume_sphere':
 			hashtag.append(math.pi * num1 ** 3 * 4 /3)
+		elif ops[i] == 'square_perimeter':
+			hashtag.append(num1 * 4)
+		elif ops[i] == 'square_edge_by_area':
+			hashtag.append(math.sqrt(num1))
+		elif ops[i] == 'square_edge_by_perimeter':
+			hashtag.append(num1 / 4)
+
 		else:
 			if i + 2 >= len(ops):
 				return ops[i]+' invalid len'
 			num1 = toNum(ops[i + 1],ns,hashtag)
 			num2 = toNum(ops[i + 2],ns,hashtag)
 			if num1 == None or num2 == None:
-				return 'number invalid'
+				return 'number invalid2 ' + ops[i]
 		
 			if ops[i] == 'add':
 				hashtag.append(num1 + num2)
@@ -85,6 +104,8 @@ def caculate(op,ns):
 				except:
 					return '{} {} math range'.format(num1,num2)
 			elif ops[i] == 'reminder':
+				if num2 == 0:
+					return 'reminder 0'
 				hashtag.append(num1%num2)
 			elif ops[i] == 'rhombus_area':
 				hashtag.append(num1 * num2 / 2)
@@ -99,46 +120,89 @@ def caculate(op,ns):
 			elif ops[i] == 'rectangle_area':
 				hashtag.append(num1 * num2)
 			elif ops[i] == 'choose':
-				hashtag.append(math.factorial(num1) / math.factorial(num2) / math.factorial(num1-num2))
+				if num1 < num2:
+					return 'n2 > n1 choose {} {}'.format(num1,num2)
+				if num1 > 10 ** 3:
+					return 'fac big num {}'.format(num1)
+				hashtag.append(math.factorial(num1) / math.factorial(num2) / math.factorial(num1 - num2))
 			elif ops[i] == 'volume_cylinder':
 				hashtag.append(math.pi * num1 * 2 * num2)
+			elif ops[i] == 'surface_cylinder':
+				hashtag.append(math.pi * 2 * num1** 2 + math.pi * 2 * num1 * num2)
+			elif ops[i] == 'speed':
+				hashtag.append(num1 / num2)
+			elif ops[i] == 'stream_speed':
+				hashtag.append((num1 + num2) / 2)
+			elif ops[i] == 'rectangle_perimeter':
+				hashtag.append(2 * (num1 + num2))
+			elif ops[i] == 'max':
+				hashtag.append(max(num1,num2))
+			elif ops[i] == 'min':
+				hashtag.append(min(num1,num2))
+			elif ops[i] == 'permutation':
+				hashtag.append(math.factorial(num1) / math.factorial(num2))
+
 			else:
 				if i + 3 >= len(ops):
 					return ops[i]+' invalid len'
 				num3 = toNum(ops[i + 3],ns,hashtag)
+				if num3 == None:
+					return 'number invalid3 ' + ops[i]
 				if ops[i] == 'surface_rectangular_prism':
 					hashtag.append((num1*num2 + num1 * num3 + num2 * num3) * 2 )
 				elif ops[i] == 'volume_rectangular_prism':
 					hashtag.append(num1 * num2 * num3)
+				elif ops[i] == 'quadrilateral_area':
+					hashtag.append(num1 * (num2 + num3) / 2)
+				elif ops[i] == 'triangle_area_three_edges':
+					s = (num1 + num2 + num3) / 2
+					if s < num1 or s < num2 or s < num3:
+						return 'invalid triangle'
+					hashtag.append(math.sqrt(s * (s - num1) * (s - num2) * (s - num3))) 
+				elif ops[i] == 'triangle_perimeter':
+					hashtag.append(num1 + num2 + num3)
 
 				else:
 					return ops[i]
 				i += 1
 			i += 1
 		i += 2
+		if hashtag[-1] > 10 **30:
+			return 'big num'
 	return hashtag[-1]
 
-import json
+if __name__ == "__main__":
+	import json
+	import sys
+	nlist = open('test.nlist').readlines()
+	for i in range(len(nlist)):
+		nlist[i] = json.loads(nlist[i])
 
-nlist = open('test.nlist').readlines()
-for i in range(len(nlist)):
-	nlist[i] = json.loads(nlist[i])
+	ac = 0.0
+	count = 0.0
+	errhypo = 0
+	if len(sys.argv) == 2:
+		gout = sys.argv[1]
+	else:
+		gout = 'gen.out'
+	gen = open(gout).readlines()
+	err = 0
+	for l in gen:
+		idx,tgt,hypo = l.split('\t')
+		idx = int(idx)
+		tgtnum = caculate(tgt,nlist[idx])
+		hyponum = caculate(hypo,nlist[idx])
+		if type(tgtnum) == type('asdf'):
+			print(idx,'tgt',tgtnum)
+			err += 1
+			continue
+		if type(hyponum) == type('asdf'):
+			print(idx,'hypo',hyponum)
+			errhypo += 1
 
-ac = 0.0
-count = 0.0
-gen = open('gen.out').readlines()
+		if tgtnum == hyponum:
+			ac += 1.0
 
-for l in gen:
-	idx,tgt,hypo = l.split('\t')
-	idx = int(idx)
-	tgtnum = caculate(tgt,nlist[idx])
-	hyponum = caculate(hypo,nlist[idx])
-	if type(tgtnum) == type('asdf'):
-		print(idx,'tgt',tgtnum)
-	if type(hyponum) == type('asdf'):
-		print(idx,'hypo',hyponum)
-
-	if tgtnum == hyponum:
-		ac += 1.0
-
-print(ac/len(gen))
+	print(ac/len(gen))
+	print(err)
+	print(errhypo)
--- get_ans.py
+++ get_ans.py
@@ -1,5 +1,5 @@
 from unicodedata import numeric
-from caculate_pad import caculate,toNum
+from caculate import caculate,toNum
 def get_num(num_str):
 
 	if ' ) ' in num_str:
@@ -60,38 +60,44 @@ import sys
 
 import json
 
-nlist = open('test.nlist').readlines()
-for i in range(len(nlist)):
-	nlist[i] = json.loads(nlist[i])
 
-ac = 0.0
-count = 0.0
-if len(sys.argv) == 2:
-	out = sys.argv[1]
-else:
-	out = 'gen.out'
-gen = open(out).readlines()
-problems = json.load(open('test.json'))
-error_c = 0
-err_ans = 0
-for l in gen:
-	count += 1
-	idx,tgt,hypo = l.split('\t')
-	idx = int(idx)
+def main():
+	nlist = open('test.nlist').readlines()
+	for i in range(len(nlist)):
+		nlist[i] = json.loads(nlist[i])
 
-	hyponum = caculate(tgt,nlist[idx])
-	if type(hyponum) == type('asdf'):
-		print(idx,'hypo',hyponum)
-		error_c += 1
-		continue
-	try:
-		correct, choose = get_ans(problems[idx],hyponum)
-		if correct == choose:
-			ac += 1
-	except:
-		print(idx)
-		err_ans += 1
-		continue
-print(ac/count)
-print(error_c)
-print(err_ans)
+	ac = 0.0
+	count = 0.0
+	if len(sys.argv) == 2:
+		out = sys.argv[1]
+	else:
+		out = 'gen.out'
+	gen = open(out).readlines()
+	problems = json.load(open('test.json'))
+	error_c = 0
+	err_ans = 0
+	for l in gen:
+		count += 1
+		idx,tgt,hypo = l.split('\t')
+		idx = int(idx)
+
+		hyponum = caculate(tgt,nlist[idx])
+		if type(hyponum) == type('asdf'):
+			print(idx,'hypo',hyponum)
+			error_c += 1
+			continue
+		try:
+			correct, choose = get_ans(problems[idx],hyponum)
+			if correct == choose:
+				ac += 1
+			else:
+				print('error ans',idx)
+		except:
+			print(idx)
+			err_ans += 1
+			continue
+	print(ac/count)
+	print(error_c)
+	print(err_ans)
+if __name__ == '__main__':
+	main()
