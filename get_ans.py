cba01612778bde124420a284a49b1e1f8c9a7392
St-Ren <b06705009@ntu.edu.tw>
2020-04-24 12:33:59 +0800

    get_ans_fin 1

--- get_ans.py
+++ get_ans.py
@@ -1,5 +1,6 @@
 from unicodedata import numeric
 from caculate import caculate,toNum
+from word2number import w2n
 def get_num(num_str):
 
 	if ' ) ' in num_str:
@@ -38,19 +39,31 @@ def get_num(num_str):
 
 def try_get_num(num_str):
 	if ' ) ' in num_str:
-		return get_num(num_str.split(' ) ')[1])
+		return try_get_num(num_str.split(' ) ')[1])
 	if 'rs .' in num_str:
-		return get_num(num_str.split('rs .')[1])
+		return try_get_num(num_str.split('rs .')[1])
 	
 	num_str = num_str.replace('. ','.')
 	num_str = num_str.replace('. ','.')
 	num_str = num_str.replace(',','')
 	if '/' in num_str:
 		n1,n2 = num_str.split('/')
-		return try_get_num(n1)/try_get_num(n2)
+		n1 = try_get_num(n1)
+		n2 = try_get_num(n2)
+		if n1 == None:
+			return 0
+		elif n2 == None:
+			return None
+		return n1/n2
 	if ':' in num_str:
 		n1,n2 = num_str.split(':')
-		return try_get_num(n1)/ try_get_num(n2) 
+		n1 = try_get_num(n1)
+		n2 = try_get_num(n2)
+		if n1 == None:
+			return 0
+		if n2 == 0 or n2 == None:
+			return None
+		return n1/n2 
 	for i,num in enumerate(['one','two','three','four','five','six']):
 		if num in num_str.lower():
 			return i + 1
@@ -62,33 +75,43 @@ def try_get_num(num_str):
 				return rt
 			except ValueError:
 				continue
-	if 'none' in num_str.lower():
+	if 'none' in num_str.lower() or 'no ' in num_str.lower():
 		return None
-
-	return 'error_ans {}'.format(num_str)
+	try:
+		w2n.word_to_num(num_str)
+	except ValueError:
+		print('error_ans {}'.format(num_str))
+		return None
 
 
 
 def get_ans(problem,hypo):
-	try:
-		json.loads
-		##json type ans
-	try:
-		ops = [get_num(op[4:]) for op in problem['options'].split(' , ')]
-	except:
-		print('get_ans err')
-		return 'err_ans:','err'
+	if problem['options'][1] == "'":
+		ops = json.loads(problem['options'].replace("'",'"'))
+	else:
+		ops = problem['options'].split(' , ')
+	#try:
+	ops = [try_get_num(op[4:]) for op in ops]
+	#except:
+	#	print('get_ans err',problem['options'])
+	#	return 'err_ans:','err'
 	correct = ord(problem['correct']) - ord('a')
+	if ops.count(None) == 5:
+		print('all_none')
+		return 'all','none'
+
 	choose = 0
+	c_value = 2147483647
 	have_none = -1
 	for i in range(5):
 		if ops[i] == None:
 			have_none = i
 			continue
-		if abs(hypo - ops[i]) < abs(hypo - ops[choose]):
+		if abs(hypo - ops[i]) < c_value:
 			choose = i
+			c_value = abs(hypo - ops[i])
 	if have_none >= 0:
-		if abs(hypo - ops[choose]) > 1 or abs((hypo - ops[choose])/ops[choose]) > 0.01:
+		if abs(hypo - ops[choose]) > 1 or (int(ops[choose]) != ops[choose] and abs((hypo - ops[choose])/ops[choose]) > 0.01):
 			choose = have_none
 	return correct, choose
 
